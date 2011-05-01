654df0c1eebbb24f4cf40b82547312e6f3a1410a
Mikejoro <mjr127@case.edu>
2011-05-01 14:46:35 -0400

    fixed issue with being able to control pause menu when in text screen (test yourself to make sure)

--- Project290/Project290/Project290/Screens/PauseScreen.cs
+++ Project290/Project290/Project290/Screens/PauseScreen.cs
@@ -130,9 +130,6 @@ namespace Project290.Screens
                 GameClock.Unpause();
                 this.Disposed = true;
             }
-
-            this.menu.position = this.position.Value;
-            this.menu.Update();
             this.background.Set(this.position.X.Value, this.position.Y.Value);
             this.background.Update();
         }
